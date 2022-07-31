d4c03640e1aed39e44a995173e62f7f51bbf571b
hatemmahoud12 <105804651+hatemmahoud12@users.noreply.github.com>
2022-07-31 19:51:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function ar(){
+    document.getElementById("logo").innerHTML="ح<span>اتم</span>";
+    document.getElementById("1").innerHTML="المهارات";
+    document.getElementById("2").innerHTML="حول";
+    document.getElementById("3").innerHTML="تواصل";
+    document.getElementById("4").innerHTML="مشاريع";
+    document.getElementById("body").style.direction="rtl";
+    document.getElementById("h3").innerHTML="<span>اهلا,</span> انا اسمي:";
+    document.getElementById("h1").innerHTML="حاتم <span>محمود</span>";
+    document.getElementById("h2").innerHTML="مصمم مواقع";
+    document.getElementById("cv").innerHTML="تحميل السيره الذاتيه";
+    document.getElementById("ski").innerHTML="المهارات";
+    document.getElementById("photo").innerHTML="تصميم الصور";
+    
+        document.getElementById("title1").innerHTML="مشاريع";
+
+        document.getElementById("ab").innerHTML="حول";
+
+        document.getElementById("abp").innerHTML="انا حاتم عندي 14 سنه.لدي خبره في تصميم وبرمجه المواقع الالكترونيه بطريقه منظمه وبشكل احترافي وايضا متوافقه مع جميع الشاشات.خبرتي في هذا المجال 3سنوات اما في تصميم الصور سنتين. ولدي خبره في مجالات اخري ولكن هذه اشهرهم";
+
+        document.getElementById("con").innerHTML="تواصل معي";
+
+        document.getElementById("clock").innerHTML="من الساعة 11 صباحاً حتى 3 صباحاً";
+
+        document.getElementById("page").innerHTML="صفحة المواقع الإلكترونية - حاتم محمود";
+            document.getElementById("icon").style.marginLeft="22px";
+                document.getElementById("icon1").style.marginLeft="22px";
+            document.getElementById("icon2").style.marginLeft="22px";
+    
+            document.getElementById("apps").innerHTML="تطبيقات سطح مكتب";
+            document.getElementById("games").innerHTML="العاب سطح مكتب";
+            document.getElementById("games").style.fontSize="14px";
+            document.getElementById("apps").style.fontSize="12.5px";
+            document.getElementById("apps").style.fontWeight="600";
+
+}
